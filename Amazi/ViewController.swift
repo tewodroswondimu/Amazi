872b9ec33b7dc7f5ce18f1e2567268fa45888788
Tewodros Wondimu <tewodros.wr@gmail.com>
2017-10-31 13:37:45 +0200

    Initial Commit

--- Amazi/ViewController.swift
+++ Amazi/ViewController.swift
@@ -7,12 +7,26 @@
 //
 
 import UIKit
+import ARKit
 
 class ViewController: UIViewController {
-
+    // Outlets for the different elements
+    @IBOutlet weak var menuCollectionView: UICollectionView!
+    @IBOutlet weak var statusViewBar: UIView!
+    @IBOutlet weak var sceneView: ARSCNView!
+    
+    // Configuration
+    let configuration = ARWorldTrackingConfiguration()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // enable horizontal plane detection
+        configuration.planeDetection = .horizontal;
+        
+        // Run the configuration on the sceneView Session
+        self.sceneView.session.run(configuration);
+        
     }
 
     override func didReceiveMemoryWarning() {
