77f1d7475c0f3e97ea7e2fa1c2bcb3cf51b20f40
Tewodros Wondimu <tewodros.wr@gmail.com>
2017-11-01 12:43:41 +0200

    added pinch gesture to scale objects in the scene

--- Amazi/ViewController.swift
+++ Amazi/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 import ARKit
 
 class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, ARSCNViewDelegate {
+    
     // All the menu items
     let menuArray: [String] = ["Well", "Drip", "Solar", "vase"]
     var selectedItem: String?
@@ -18,6 +19,7 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     @IBOutlet weak var menuCollectionView: UICollectionView!
     @IBOutlet weak var statusViewBar: UIView!
     @IBOutlet weak var sceneView: ARSCNView!
+    @IBOutlet weak var statusLabel: UILabel!
     
     // Configuration
     let configuration = ARWorldTrackingConfiguration()
@@ -28,6 +30,8 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         // Setup the data source and delegate of the collection view
         self.menuCollectionView.dataSource = self
         self.menuCollectionView.delegate = self
+        
+        self.sceneView.delegate = self
         
         // show feature points and the world origin when the application loads up
         sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints];
@@ -44,8 +48,50 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     
     
     func registerGestureRecognizer() {
+        
+        // Allows to detect tap gestures
         let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tapped))
+        
+        // Allows to detect pinch gestures 
+        let pinchGestureRecognizer = UIPinchGestureRecognizer(target: self, action: #selector(pinched))
+        
         self.sceneView.addGestureRecognizer(tapGestureRecognizer)
+        self.sceneView.addGestureRecognizer(pinchGestureRecognizer)
+    }
+    
+    // method for when a person pinches on the screen
+    @objc func pinched(sender: UIPinchGestureRecognizer) {
+        // get the sceneview that was pinched on
+        let sceneView = sender.view as! ARSCNView
+        
+        // location that was pinched in the scene view
+        let pinchLocation = sender.location(in: sceneView)
+        
+        // check wheter your pinch matches the location of an object
+        let hitTest = sceneView.hitTest(pinchLocation)
+        
+        // Check if an object was pinched
+        if (!hitTest.isEmpty) {
+            // get the result from the hit test
+            let results: SCNHitTestResult = hitTest.first!
+            
+            // get the node from the result
+            let node = results.node
+            
+            // based on how far the person pinched on the screen scale the object immediately
+            let pinchAction = SCNAction.scale(by: sender.scale, duration: 0)
+            
+            // run the pinch action on the node
+            node.runAction(pinchAction)
+            
+            // Make the scale size constant, so that it doesn't keep gettting bigger
+            sender.scale = 1.0
+            
+            // go to the scene and click on flatten node if you have a scn object that has children
+        }
+        else {
+            print("The pinch hit test did not work")
+        }
     }
     
     // taps on a plane
@@ -119,6 +165,15 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
         guard anchor is ARPlaneAnchor else {return}
+        
+        // renderer is done in a separate thread so run everything that has to do with UI in the main queue
+        DispatchQueue.main.async {
+            self.statusLabel.isHidden = false
+            self.statusLabel.text = "Plane detected"
+            DispatchQueue.main.asyncAfter(deadline: .now()+3) {
+                self.statusLabel.isHidden = true
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
